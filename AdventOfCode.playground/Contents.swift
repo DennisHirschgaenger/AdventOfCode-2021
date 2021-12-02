a86ece8344bcb0d5e7ec8bb0bf5216e7fe8843f7
Dennis Hirschgänger <dennis.hirschgaenger@tomorrow.one>
2021-12-02 12:17:47 +0100

    Set up basic structure for playground, added gitignore

--- AdventOfCode.playground/Contents.swift
+++ AdventOfCode.playground/Contents.swift
@@ -1,6 +1,30 @@
 import Foundation
 
+public enum Day<#X#> {
+    public static func puzzle1() -> Int {
+        let input = loadInput()
+        return 0
+    }
+
+    static func loadInput() -> [Int] {
+        guard let fileURL = Bundle.main.url(forResource: "input-day<#X#>", withExtension: "txt"),
+        let input = try? String(contentsOf: fileURL, encoding: .utf8) else {
+            fatalError("Failed to load input")
+        }
+        return input.trimmingCharacters(in: .whitespacesAndNewlines)
+            .components(separatedBy: .controlCharacters)
+            .compactMap(Int.init)
+    }
+}
+
+public extension Day1 {
+    static func puzzle2() -> Int {
+        let input = loadInput()
+        return 0
+    }
+}
+
 let start = Date()
-let result = Day1.puzzle1()
+let result = Day<#X#>.puzzle1()
 print(result)
 print("Execution time: \(Date().timeIntervalSince(start))")
